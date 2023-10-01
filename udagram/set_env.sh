56bc846f4f2358aee2be6653e3edfb5a26163e29
haunt1603 <haunt1603@gmail.com>
2023-10-02 00:25:03 +0700

    update

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,13 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME= postgres
-export POSTGRES_PASSWORD= postgres123!
-export POSTGRES_HOST= postgres.cgiwanscgppv.us-east-1.rds.amazonaws.com
-export POSTGRES_DB= postgres
-export AWS_ACCESS_KEY_ID= ASIA5UOJQJWQA27ZKMGR
-export AWS_SECRET_ACCESS_KEY= 27gGHlyzNENCwb3TbbR6loMNI7E2b8WCh8BBb8iP
-export AWS_BUCKET= arn:aws:s3:::bucket9372
-export AWS_REGION= us-east-1
-export AWS_PROFILE= default
-export JWT_SECRET= mysecretstring
-export URL= http://localhost:8080
+setx POSTGRES_USERNAME= postgres
+setx POSTGRES_PASSWORD= postgres123!
+setx POSTGRES_HOST= postgres.cgiwanscgppv.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB= postgres
+setx AWS_ACCESS_KEY_ID= ASIA5UOJQJWQA27ZKMGR
+setx AWS_SECRET_ACCESS_KEY= 27gGHlyzNENCwb3TbbR6loMNI7E2b8WCh8BBb8iP
+setx AWS_BUCKET= arn:aws:s3:::bucket9372
+setx AWS_REGION= us-east-1
+setx AWS_PROFILE= default
+setx JWT_SECRET= mysecretstring
+setx URL= http://localhost:8080
